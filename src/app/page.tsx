9498dbaa26ced3f87ca49b4e8a9477e7ef9f67ba
kumavis <aaron@kumavis.me>
2023-06-29 16:52:47 -1000

    mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 import styles from './page.module.css'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { ChatGPTAPI } from 'chatgpt'
 
 const preprompt =
 `
-Respond only with a single HTML doc.
+Respond only with a single HTML fragment to be placed inside a <body>.
 Do not respond with any other content.
-Do not put the HTML doc in a frame.
+Do not put the HTML in a frame.
 Do not reference any external resources.
+You may use javascript to allow the user easily ask premade follow up questions.
+\`aiPrompt(promptString)\` will be available in the global scope.
+If your answer contains a list (<ol>), you should ALWAYS use \`aiPrompt\` to provide follow up questions digging deeper into the answer.
+For example, if the prompt is "how to fix a car", you may respond with "<ol><li><a href="javascript:aiPrompt('how to change the oil?')">change the oil"</a></li><li><a href="javascript:aiPrompt('how to change the tires?')">change the tires</a></li></ol>".
+Only call \`aiPrompt\` in response to a user interaction.
+
 In the html doc, respond to the following query:
+`
+
+const docFormat =
+`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>hyper prompt</title>
+  </head>
+  <body>
+    __RESPONSE__
+  </body>
+</html>
 `
 
 export default function Home() {
@@ -22,17 +42,43 @@ export default function Home() {
     // get and reset prompt
     const prompt = promptInput
     setPromptInput('')
+    processPrompt(prompt)
+  }
+
+  const processPrompt = async (prompt: string) => {
     // ask ai
     const apiKey = new URLSearchParams(location.search).get('apiKey')
-    const api = new ChatGPTAPI({ apiKey })
+    if (!apiKey) throw new Error('apiKey not found in query string')
+    const api = new ChatGPTAPI({
+      apiKey,
+      completionParams: {
+        model: 'gpt-4',
+        // temperature: 0.5,
+        // top_p: 0.8
+      }
+    })
     // workaround for https://github.com/transitive-bullshit/chatgpt-api/issues/592
     api._fetch = self.fetch.bind(self)
     console.log(`prompt: ${prompt}`)
     const formattedPrompt = `${preprompt}${prompt}`
     const res = await api.sendMessage(formattedPrompt)
     console.log(res.text)
-    setPageDoc(res.text)
+    // format response
+    const formattedPageDoc = docFormat.replace('__RESPONSE__', res.text)
+    setPageDoc(formattedPageDoc)
   }
+
+  const updateIframeDoc = useCallback((iframeElement) => {
+    const frameGlobal = iframeElement.contentWindow
+    console.log('preparing aiPrompt')
+    frameGlobal.aiPrompt = (newPrompt) => {
+      const accepted = confirm(`the ai prompt is:\n${newPrompt}`)
+      if (accepted) {
+        processPrompt(newPrompt)
+      }
+    }
+  }, [])
+
 
   return (
     <main className={styles.main}>
@@ -41,7 +87,11 @@ export default function Home() {
         <h1 className={styles.title}>hyper prompt</h1>
       </div>
       
-      <iframe className={styles.iframe} srcDoc={pageDoc} />
+      <iframe
+        className={styles.iframe}
+        srcDoc={pageDoc}
+        onLoad={(event) => updateIframeDoc(event.target)}
+      />
       
       <div className={styles.description}>
         <form onSubmit={submitPrompt}>
