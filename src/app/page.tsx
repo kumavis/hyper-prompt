91166e280a73c68fc53ed87a6d6203dcce070dcb
kumavis <aaron@kumavis.me>
2023-06-29 16:07:42 -1000

    prompt mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,54 @@
-import Image from 'next/image'
+"use client"
+
 import styles from './page.module.css'
+import { useState } from 'react'
+import { ChatGPTAPI } from 'chatgpt'
+
+const preprompt =
+`
+Respond only with a single HTML doc.
+Do not respond with any other content.
+Do not put the HTML doc in a frame.
+Do not reference any external resources.
+In the html doc, respond to the following query:
+`
 
 export default function Home() {
+  const [pageDoc, setPageDoc] = useState()
+  const [promptInput, setPromptInput] = useState('')
+
+  const submitPrompt = async (event: Event) => {
+    event.preventDefault();
+    // get and reset prompt
+    const prompt = promptInput
+    setPromptInput('')
+    // ask ai
+    const apiKey = new URLSearchParams(location.search).get('apiKey')
+    const api = new ChatGPTAPI({ apiKey })
+    // workaround for https://github.com/transitive-bullshit/chatgpt-api/issues/592
+    api._fetch = self.fetch.bind(self)
+    console.log(`prompt: ${prompt}`)
+    const formattedPrompt = `${preprompt}${prompt}`
+    const res = await api.sendMessage(formattedPrompt)
+    console.log(res.text)
+    setPageDoc(res.text)
+  }
+
   return (
     <main className={styles.main}>
+      
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <h1 className={styles.title}>hyper prompt</h1>
+      </div>
+      
+      <iframe className={styles.iframe} srcDoc={pageDoc} />
+      
+      <div className={styles.description}>
+        <form onSubmit={submitPrompt}>
+          <input className={styles.input} value={promptInput} onChange={(e) => setPromptInput(e.target.value)} />
+        </form>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
